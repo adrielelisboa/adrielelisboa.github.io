a455ee7da1ae65cb99b7064edc04fc0ed4848602
Realiza <realiza@Realiza-5.local>
2017-12-05 15:09:23 -0200

    modifiquei o trabalho

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
- $(document).ready(function() { 
-    
-        $("#imgMenu1").click(function() {
+$(document).ready(function () {
+
+      $("#imgMenu1").click(function () {
             $("#listaMenu").toggle();
       });
 
-      $("#imgPincel").click(function() {
+      $("#imgPincel").click(function () {
             $("#pinceis").show();
+            $("#sombras").hide();
+            $("#blush").hide();
       });
-      $("#imgSombras").click(function() {
+      $("#imgSombras").click(function () {
             $("#sombras").show();
             $("#pinceis").hide();
+            $("#blush").hide();
       });
-    
+      $("#imgBlush").click(function () {
+            $("#blush").show();
+            $("#pinceis").hide();
+            $("#sombras").hide();
       });
+});
