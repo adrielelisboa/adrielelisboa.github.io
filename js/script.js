0033ce84957677b5021bf3a5656f20dfbb878f56
Realiza <realiza@Realiza-5.local>
2017-12-06 16:56:05 -0200

    Modifiquei o trabalho

--- js/script.js
+++ js/script.js
@@ -8,15 +8,48 @@ $(document).ready(function () {
             $("#pinceis").show();
             $("#sombras").hide();
             $("#blush").hide();
+            $("#batom").hide();
+            $("#esmaltes").hide();
+            $("#perfumes").hide();
       });
       $("#imgSombras").click(function () {
             $("#sombras").show();
             $("#pinceis").hide();
             $("#blush").hide();
+            $("#batom").hide();
+            $("#esmaltes").hide();
+            $("#perfumes").hide();
       });
       $("#imgBlush").click(function () {
             $("#blush").show();
             $("#pinceis").hide();
             $("#sombras").hide();
+            $("#batom").hide();
+            $("#esmaltes").hide();
+            $("#perfumes").hide();
+      });
+      $("#imgBatom").click(function () {
+            $("#batom").show();
+            $("#blush").hide();
+            $("#pinceis").hide();
+            $("#sombras").hide();
+            $("#esmaltes").hide();
+            $("#perfumes").hide();
+      });
+      $("#imgEsmalte").click(function () {
+            $("#esmaltes").show();
+            $("#batom").hide();
+            $("#blush").hide();
+            $("#pinceis").hide();
+            $("#sombras").hide();
+            $("#perfumes").hide();
+      });
+      $("#imgPerfumes").click(function () {
+            $("#perfumes").show();
+            $("#batom").hide();
+            $("#blush").hide();
+            $("#pinceis").hide();
+            $("#sombras").hide();
+            $("#esmaltes").hide();
       });
 });
